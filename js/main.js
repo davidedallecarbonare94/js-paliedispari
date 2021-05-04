d7bb6c0e2d4d585d544e999c0c4192bbbfe6fcd1
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-05-04 15:21:53 +0200

    add class correction

--- js/main.js
+++ js/main.js
@@ -1,14 +1,42 @@
-/* //Es 1. Palindroma o no?
+//Es 1. Palindroma o no?
 //Chiedere all’utente di inserire una parola 
-var userWord = prompt("Inserisci una parola");
+/* var userWord = prompt("Inserisci una parola"); */
 
-//Creare una funzione per capire se la parola inserita è tutta minuscola
-function isThisLower(userWord) {
-    var thisIsLower = userWord.toLowerCase();
-    return thisIsLower
-}
- */
 //Creare una funzione per capire se la parola inserita è palindroma
+
+
+// CORREZIONE
+
+/* function isPalindrome(word) {
+    //Rimuovere punteggiatura e spaziatura, 
+    //il metodo qua sotto mi mostra tutto ciò che non è carattere leggibile e me lo restituisce in array
+    //viene rimossa la punteggiatura
+    var hasPuntuation = "word".match(/\W/gm)
+    if (hasPuntuation !== null) {
+        for (var i = 0; i < hasPuntuation.length; i++){
+            var element = hasPuntuation[i]
+            if (word.includes(element)) {
+                word = word = word.replace(element, "") //il doppio word è per far si che poi resti il valore senza le punteggiature come valore della funzione
+                //senza il doppio word mi mostrerebbe tutte le casistiche della parola ognuna senza uno spazio o una virgola in successione
+            }
+        }
+    }
+    
+
+    var reverseWord = word.split("").reverse().join("");
+    
+    if (reverseWord.toLowerCase() === word.toLowerCase()) {
+        return true;
+    }
+    return false;
+}
+
+var userWord = prompt("Inserisci una parola");
+console.log(isPalindrome(userWord));
+
+
+ */
+
 
 
 //Pari e Dispari:
@@ -18,12 +46,12 @@ var userNumber = Number.parseInt(prompt("Inserisci un numero da 1 a 5"))
 console.log(userNumber);
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 function pcRandomNumber(min, max) {
-    var pcChoose = Math.floor(Math.random() * (max - min));
+    var pcChoose = Math.floor(Math.random() * (max - min +1) )+ min;
     return pcChoose;
 }
 var pcNumber = pcRandomNumber(1, 5)
 console.log(pcNumber);
- //Sommiamo i due numeri
+ //Sommiamo i due numeri // qui poteva essere risolto con una nuova variabile senza funzione
 function sum(myNumber, pcChooseNumber) {
     var result = myNumber + pcChooseNumber;
     return result
@@ -36,8 +64,11 @@ function isThisEven(odd, even) {
     if (somma % 2 === 0) {
         console.log(odd);
     } else {
-        console.log(even);;
+        console.log(even);
     }
 } 
 isThisEven("Ha vinto pari!", "Ha vinto dispari!")
+// avendola chiamata "is" viene solitamente dato risultato booleano
+
+
 //Dichiariamo chi ha vinto.
